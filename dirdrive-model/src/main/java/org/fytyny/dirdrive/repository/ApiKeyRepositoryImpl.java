5d3676cb4945f541f5b4e749c2a229b96c3648f8
Fytyny <marcin12332@gmail.com>
2019-02-06 21:10:26 +0100

    Refactoring, moved repository to model

---
+++ dirdrive-model/src/main/java/org/fytyny/dirdrive/repository/ApiKeyRepositoryImpl.java
@@ -0,0 +1,45 @@
+package org.fytyny.dirdrive.repository;
+
+import lombok.Getter;
+import org.fytyny.dirdrive.model.ApiKey;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.UUID;
+
+@Model
+public class ApiKeyRepositoryImpl implements ApiKeyRepository {
+
+    @Getter
+    @Inject
+    EntityManager entityManager;
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public ApiKey getById(UUID id) {
+        return entityManager.find(ApiKey.class,id);
+    }
+
+    public ApiKey save(ApiKey object) {
+        if (object == null) throw new IllegalArgumentException();
+        if (getById(object.getId()) == null){
+            entityManager.persist(object);
+            return object;
+        } else {
+            return entityManager.merge(object);
+        }
+    }
+
+    public ApiKey getByToken(String token) {
+        if (token == null) return null;
+        Query query = entityManager.createQuery("SELECT s from ApiKey s where s.token = :token");
+        query.setParameter("token",token);
+        List resultList = query.getResultList();
+        return resultList.isEmpty() ? null : (ApiKey) resultList.get(0);
+    }
+}
---
+++ dirdrive-model/src/test/java/org/fytyny/dirdrive/repository/ApiKeyRepositoryIT.java
@@ -0,0 +1,51 @@
+package org.fytyny.dirdrive.repository;
+
+import org.fytyny.dirdrive.model.ApiKey;
+import org.fytyny.dirdrive.model.Session;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.UUID;
+
+public class ApiKeyRepositoryIT {
+
+    @Rule
+    public SessionFactoryRule sessionFactoryRule = new SessionFactoryRule();
+    ApiKeyRepositoryImpl apiKeyRepository;
+
+    @Before
+    public void init(){
+        apiKeyRepository = new ApiKeyRepositoryImpl();
+        sessionFactoryRule.injectManager(apiKeyRepository);
+    }
+
+    @Test
+    public void saveTest(){
+        ApiKey apiKey = generateRandomApiKey();
+        apiKeyRepository.save(apiKey);
+        Assert.assertEquals(apiKey,apiKeyRepository.getById(apiKey.getId()));
+    }
+
+    @Test
+    public void getByTokenTest(){
+        ApiKey apiKey = generateRandomApiKey();
+        apiKeyRepository.entityManager.getTransaction().begin();
+        apiKeyRepository.save(apiKey);
+        apiKeyRepository.entityManager.getTransaction().commit();
+
+        ApiKey byToken = apiKeyRepository.getByToken(apiKey.getToken());
+        Assert.assertNotNull(byToken);
+        Assert.assertEquals(apiKey,byToken);
+    }
+
+    private ApiKey generateRandomApiKey(){
+        ApiKey apiKey = new ApiKey();
+        apiKey.setId(UUID.randomUUID());
+        apiKey.setToken(Session.generateRandom(10));
+        apiKey.setDirectoryList(new LinkedList<>());
+        return apiKey;
+    }
+}
