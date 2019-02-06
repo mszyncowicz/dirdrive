34fbad6f762f794c9a0eb5912f1d3c90eab7e0eb
marcin_2 <marcin.szyncowicz@finsafe.io>
2019-02-06 21:23:22 +0100

    Removed files

--- dirdrive-server/src/main/java/org/fytyny/dirdrive/repository/ApiKeyRepository.java
+++
@@ -1,7 +0,0 @@
-package org.fytyny.dirdrive.repository;
-
-import org.fytyny.dirdrive.model.ApiKey;
-
-public interface ApiKeyRepository extends Repository<ApiKey> {
-    ApiKey getByToken(String token);
-}
--- dirdrive-server/src/main/java/org/fytyny/dirdrive/repository/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.fytyny.dirdrive.repository;
-
-import org.fytyny.dirdrive.model.Session;
-
-public interface SessionRepository extends Repository<Session> {
-    Session getByToken(String token);
-}
